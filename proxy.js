8ab78dda7ac96749e836ab8711d0eb32c968e0df
DeroXP <96676237+DeroXP@users.noreply.github.com>
2024-04-21 00:37:37 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,27 +1,17 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
-const readline = require('readline');
 
 const proxy = httpProxy.createProxyServer({});
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 const server = http.createServer((req, res) => {
-  rl.question('Please enter the URL you want to visit (or type "skip" to go to https://www.perplexity.ai/): ', (answer) => {
-    const targetURL = 'https://www.perplexity.ai/'
-
-    console.log('Proxying request for:', req.url);
-    proxy.web(req, res, {
-      target: targetURL,
-      secure: true,
-      changeOrigin: true
-    });
-
-    console.log('Connected to site:', targetURL);
+  console.log('Proxying request for:', req.url);
+  proxy.web(req, res, {
+    target: 'https://www.perplexity.ai/',
+    secure: true,
+    changeOrigin: true
   });
+
+  console.log('Connected to site: https://www.perplexity.ai/');
 });
 
 server.listen(3000, () => {
